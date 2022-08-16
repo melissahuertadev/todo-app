b6ab676ecbff59f7bfdce75a997275191b3825ed
piratelicorne <thecodeloops@gmail.com>
2022-08-16 13:36:12 -0500

    amplify config, backend setup, ui-vue plugin

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import AmplifyVue from '@aws-amplify/ui-vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(AmplifyVue);
+app.mount('#app');
