32c7f6b67f276bce7608d93ecd8e1b18cd1a8a72
piratelicorne <thecodeloops@gmail.com>
2022-08-17 03:30:47 -0500

    frontend: fontawesome icon

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,19 @@
 <template>
   <div id="app">
     <h1>ToDo</h1>
-    <div class="wrapper">
-      <input type="text" v-model="name" placeholder="Task name">
-      <input type="text" v-model="description" placeholder="Task description">
-      <button v-on:click="addTask">Add</button>
-    </div>
-    
-    <div class="task" v-for="task in tasks" :key="task.id">
-      <h4>{{ task.name }}</h4>
-      <p>{{ task.description }}</p>
-    </div>
+    <Container />
   </div>
+
 </template>
 
 <script>
-import { API } from 'aws-amplify';
-import { createTodo } from './graphql/mutations';
-import { listTodos } from './graphql/queries';
+import Container from './components/Container'
 
 export default {
-  name: 'app',
-  async created(){
-    this.getTasks();
-    this.suscribe();
+  name: 'App',
+  components: {
+    Container,
   },
-  data() {
-    return {
-      name: '',
-      description: '',
-      tasks: []
-    }
-  },
-  methods: {
-    async addTask() {
-      const { name, description } = this;
-      if (!name || !description)
-        return;
-      
-      const task = { name, description };
-      this.tasks = [...this.tasks, task];
-      await API.graphql({
-        query: createTodo,
-        variables: {input: task},
-      });
-      this.name = '';
-      this.description = '';
-    },
-    async getTasks() {
-      const tasks = await API.graphql({
-        query: listTodos
-      });
-      this.tasks = tasks.data.listTodos.items;
-    }
-  }
 }
 </script>
 
@@ -67,31 +27,5 @@ export default {
   margin-top: 60px;
   max-width: 420px;
   margin: 0 auto;
-}
-
-button, input {
-  border-radius: 5px;
-  padding: 5px 10px;
-  border: 1px solid #665959;
-  margin: 5px;
-  color:#665959;
-}
-
-.wrapper {
-  display: flex;
-}
-
-.wrapper input {
-  flex: 1;
-}
-
-.task {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  background-color: #EEE;
-  border-radius: 5px;
-  margin: 5px 10px;
-  padding: 5px 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,30 @@ import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+// import 'element-plus/lib/theme-chalk/index.css'
 import App from './App.vue'
+
+
 import AmplifyVue from '@aws-amplify/ui-vue';
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faTrash)
+
+
 const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(ElementPlus);
 app.use(AmplifyVue);
 app.mount('#app');
